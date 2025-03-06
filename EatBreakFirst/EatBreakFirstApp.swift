e7527ac892b7b6536e75342e9aa0820d3181148f
masheal2333 <132886930+masheal2333@users.noreply.github.com>
2025-03-06 17:16:50 +0800

    Initial Commit

---
+++ EatBreakFirst/EatBreakFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatBreakFirstApp.swift
+//  EatBreakFirst
+//
+//  Created by Sheng Ma on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct EatBreakFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
